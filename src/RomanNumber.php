abd4c128fbdee357b893380d2793bc1293ab744d
manuelblanch <manuelblanch@iesebre.com>
2016-11-29 14:12:00 +0100

    pujada4

--- src/RomanNumber.php
+++ src/RomanNumber.php
@@ -1,18 +1,13 @@
 <?php
-class RomanNumbers{
-    public function convert($number){
-        if ($number==10){
+class RomanNumbers
+{
+    public function convert($number) {
+        if ($number == 10) {
             return "X";
         }
-        if ($number==5){
+        if ($number == 5) {
             return "V";
         }
-        if ($number==3){
-            return "III";
-        }
-        if ($number==2){
-            return "II";
-        }
-        return "I";
+        return str_repeat('I',$number);
     }
 }
--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -1,45 +1,28 @@
 <?php
-
-class RomanNumbers
-{
+class RomanNumbers{
     protected $code = [
-        1000 => 'M',
-        900 => 'CM',
-        500 => 'O',
-        400 => 'CO',
-        100 => 'C',
-        50 => 'L',
-        40 => 'XL',
-        10 => 'X',
-        9 => 'IX',
-        5 => 'V',
-        4 => 'IV',
-        1 => 'I'
+        1000=>"M",
+        900=>"CM",
+        500=>"D",
+        400=>"CD",
+        100 =>"C",
+        90 => "XC",
+        50 => "L",
+        40 => "XL",
+        10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
     ];
-
     public function convert($number){
-
         $solution = "";
-
-        foreach ($this ->code as $decimal => $roman){
+        foreach ($this->code as $decimal => $roman){
             if ($number >= $decimal){
                 $solution .= $roman;
-                $number .= $decimal;
+                $number -= $decimal;
             }
         }
-
-        if ($number ->$this->code(0) ->key {
-            $solution= $this->code(0) ->value;
-            $number= $this->code(0) ->key;
-        }
-
-        if ($number==4) {
-            $solution= $this->code ($number);
-            $number=0;
-        }
-
-       $solution = $solution .str_repeat('I',$number);
         return $solution;
     }
 }
-?>
